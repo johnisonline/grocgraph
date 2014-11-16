648b78c18ff61af5f6f1d2af6c04b9d848b5c9d1
johnisonline <johnisonline@gmail.com>
2014-11-16 18:02:18 -0500

    --all

---
+++ neo4jsample/src/main/java/com/john/Neo4jSample/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.john.Neo4jSample;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, String> {
+
+    Person findByName(String name);
+
+    Iterable<Person> findByTeammatesName(String name);
+
+}
